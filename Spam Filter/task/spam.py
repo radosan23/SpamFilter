9852bbf60e439a1f2db3db3b1d6cd8355cb55db7
radosan <radoslaw.sanetra@gmail.com>
2024-10-20 15:02:02 +0200

    implemented bag of words model

--- Spam Filter/task/spam.py
+++ Spam Filter/task/spam.py
@@ -14,15 +14,39 @@ def preprocess(text, model):
     return ' '.join(tokens)
 
 
+def train_test_split(df, train_ratio=0.8, random=None):
+    df = df.sample(frac=1, random_state=random, ignore_index=True)
+    train_last_index = int(df.shape[0] * train_ratio)
+    return df[:train_last_index], df[train_last_index:]
+
+
+def make_vocabulary(df):
+    result = set()
+    for text in df['SMS']:
+        result.update(text.split())
+    return sorted(result)
+
+
+def bag_of_words(df, vocabulary):
+    new_cols = []
+    for token in vocabulary:
+        new_cols.append(pd.Series(df['SMS'].apply(str.count, args=(token,)), name=token, dtype=int, index=df.index))
+    return pd.concat([df, *new_cols], axis=1)
+
+
 def main():
     df = pd.read_csv('../data/spam.csv', header=0, names=['Target', 'SMS'], usecols=[0, 1],
                      encoding='iso-8859-1')
-    pd.options.display.max_columns = df.shape[1]
-    pd.options.display.max_rows = df.shape[0]
 
     model = spacy.load('en_core_web_sm')
     df.SMS = df.SMS.apply(preprocess, args=(model,))
-    print(df.head(200))
+    df_train, df_test = train_test_split(df, train_ratio=0.8, random=43)
+    vocabulary = make_vocabulary(df_train)
+    df_train_bow = bag_of_words(df_train, vocabulary=vocabulary)
+
+    pd.options.display.max_columns = df_train_bow.shape[1]
+    pd.options.display.max_rows = df_train_bow.shape[0]
+    print(df_train_bow.iloc[:200, :50])
 
 
 if __name__ == '__main__':
